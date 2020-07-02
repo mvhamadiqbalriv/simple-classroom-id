c12523e93c9b9efc4559f148bdb655af9fc8169c
mvhamadiqbalriv <a2.1800086@mhs.stmik-sumedang.ac.id>
2020-07-02 12:47:04 +0700

    move js to an external classrooms detail

--- resources/views/layouts/back.blade.php
+++ resources/views/layouts/back.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="_token" content="{{ csrf_token() }}">
     <title> @yield('title') - SCID</title>
     <!-- Favicon -->
     <link rel="icon" href="{{asset('template/back-ui/img/brand/favicon.png')}}" type="image/png">
